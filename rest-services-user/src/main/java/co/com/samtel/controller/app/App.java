5de3bd2092543be3f60adbc04148d97dfbfedaaa
Jesus Sierra <jnsierrac@gmail.com>
2018-11-13 14:34:45 -0500

    Ajustes para deploy en wildfly

--- rest-services-user/src/main/java/co/com/samtel/controller/app/App.java
+++ rest-services-user/src/main/java/co/com/samtel/controller/app/App.java
@@ -2,6 +2,8 @@ package co.com.samtel.controller.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,9 +13,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "co.com.samtel")
 @PropertySource("classpath:application.properties")
 @EnableJpaRepositories(basePackages={"co.com.samtel.repo.repository"})
-public class App {
+public class App extends SpringBootServletInitializer{
+	
+	private static Class<App> applicationClass = App.class;
 	
 	public static void main(String ... args) {
 		SpringApplication.run(App.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 }
